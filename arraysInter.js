a63028e23ed8e24a4ec4a2b3ddfeac4b4e40f925
npool1 <nicholaspool@gmail.com>
2018-06-29 10:29:03 -0700

    added skyline algo to arrayIntermediate

--- arraysInter.js
+++ arraysInter.js
@@ -1,14 +1,15 @@
-function concat(arr1, arr2){
-    for (var i = 0; i<arr2.length; i++){
-        arr1[arr1.length] = arr2[i]
+function rotateArr(arr, shiftBy){
+    var temp;
+    for(var i = 0; i < arr.length; i++){
+        if (i + shiftBy <= arr.length -1){
+            temp = arr[i + shiftby]
+            arr[i + shiftby] = arr[i]
+        }else{
+            temp = arr[(arr.length-1)-shiftBy]
+        }
+
     }
-    return arr1;
 }
-
-var arr1 = [1,2,3]
-var arr2 = [-4,"pig",60000000]
-
-concat (arr1, arr2)
 
 function filterRange(arr,min,max){
     for (var i = 0; i < arr.length; i++){
@@ -27,3 +28,48 @@ var min = 2
 var max = 8
 
 filterRange(arr,min,max)
+
+function concat(arr1, arr2){
+    for (var i = 0; i<arr2.length; i++){
+        arr1[arr1.length] = arr2[i]
+    }
+    return arr1;
+}
+
+var arr1 = [1,2,3]
+var arr2 = [-4,"pig",60000000]
+
+concat (arr1, arr2)
+
+function skyline(arr){
+
+    var insertAt;
+    
+    for (var i = 0; i < arr.length; i++){
+      console.log("insertAt: " + insertAt + "\n" + "i: " + i);
+      if(i === 0 && arr[i] > 0){
+        insertAt = i + 1
+      }
+      if (i === 0 && arr[i] <= 0){
+        insertAt = i;
+      }else{
+          if (insertAt === 0 && arr[i] > arr[insertAt]){
+            arr[insertAt] = arr[i]
+            insertAt += 1;
+            console.log(arr)
+          }
+          else if(arr[i] > arr[insertAt-1]){
+            arr[insertAt] = arr[i]
+            insertAt += 1;
+            console.log(arr)
+          }
+        }
+    } 
+    var difference = arr.length - insertAt;
+    arr.length = (arr.length) - difference;
+    return arr
+}
+
+var arr = [1,2,3,-4,5];
+skyline(arr);
+
